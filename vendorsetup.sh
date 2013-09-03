13aed2ec015572eed6d7864150207c6af06765bd
Kasper Hettinga <kasper.hettinga@gmail.com>
2013-09-03 19:24:08 +0200

    Add p4 to build targets
    Change-Id: Ic2cb28118cfa2ff7e087154c362f97f99d77a47f

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -27,6 +27,10 @@ add_lunch_combo aokp_m7ul-userdebug
 add_lunch_combo aokp_maserati-userdebug
 add_lunch_combo aokp_mint-userdebug
 add_lunch_combo aokp_odin-userdebug
+add_lunch_combo aokp_p4-userdebug
+add_lunch_combo aokp_p4tmo-userdebug
+add_lunch_combo aokp_p4vzw-userdebug
+add_lunch_combo aokp_p4wifi-userdebug
 add_lunch_combo aokp_p930-userdebug
 add_lunch_combo aokp_pollux-userdebug
 add_lunch_combo aokp_polluxwindy-userdebug
